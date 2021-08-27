bef50906c9b99633d3d31ae3f252bd5fcf897e37
Yurii Sydoriak <sydoriaky@gmail.com>
2021-08-27 20:11:39 +0300

    minor

---
+++ backend/Marx/backend/Cart/Cart.Infrastructure/Interfaces/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cart.Infrastructure.Interfaces
+{
+    public interface IGenericRepository<T> where T : class, IEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int Id);
+        Task<bool> AddAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(int Id);
+    }
+}

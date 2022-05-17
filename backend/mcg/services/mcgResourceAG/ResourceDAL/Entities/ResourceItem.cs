106b87f0eae7be80c0e371ab92f820a9fce32b9c
Yurii Sydoriak <sydoriaky@gmail.com>
2022-05-17 16:32:40 +0300

    migra

---
+++ backend/mcg/services/mcgResourceAG/ResourceDAL/Entities/ResourceItem.cs
@@ -0,0 +1,11 @@
+﻿using ResourceDAL.Interfaces;
+
+namespace ResourceDAL.Entities;
+
+public class ResourceItem: IEntity
+{
+    public Guid Id { get; set; }
+    public string FileName { get; set; }
+    public string FileUri { get; set; }
+    public string AsociatedWith { get; set; }
+}
---
+++ backend/mcg/services/mcgResourceAG/ResourceDAL/Interfaces/IEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace ResourceDAL.Interfaces;
+
+public interface IEntity
+{
+    public Guid Id { get; set; }
+}
